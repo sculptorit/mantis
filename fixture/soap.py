e9fbc6d87e99e7a3ff01cf79b41bb598bc50ed75
sculptorit <vladsmol@bk.ru>
2023-08-12 18:04:04 +0300

    Дополнение к заданию 26:

--- fixture/soap.py
+++ fixture/soap.py
@@ -1,14 +1,15 @@
-from suds import WebFault
 from suds.client import Client
+from suds import WebFault
+from model.project import Project
 
 
 class SoapHelper:
 
     def __init__(self, app):
         self.app = app
-        self.client = Client("http://localhost/mantisbt-1.2.20/api/soap/mantisconnect.php?wsdl")
 
     def iloginp(self, username, password):
+        client = Client(self.app.config['web']['baseUrl'] + "/api/soap/mantisconnect.php?wsdl")
         try:
             client.service.mc_login(username, password)
             return True
@@ -18,6 +19,14 @@ class SoapHelper:
     def get_list(self):
         username = self.app.config['webadmin']['username']
         password = self.app.config['webadmin']['password']
-        result = self.client.service.mc_projects_get_user_accessible(username, password)
-        projects = list(map(lambda x: x.id, result))
-        return projects
+        projects_list = []
+        client = Client(self.app.config['web']['baseUrl'] + "/api/soap/mantisconnect.php?wsdl")
+        try:
+            projects = client.service.mc_projects_get_user_accessible(username, password)
+            project_list = []
+            for project in projects:
+                project_list.append(Project(project_name=project.name,
+                                            project_description=project.description))
+            return project_list
+        except WebFault:
+            return None
